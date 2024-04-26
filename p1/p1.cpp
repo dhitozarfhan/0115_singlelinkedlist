da1bc6fcd56521d6fdd429cf954d7e8b07d9d85d
dhitozarfhan <dhitozarfhan@gmail.com>
2024-04-26 08:57:32 +0700

    void addNode

--- p1/p1.cpp
+++ p1/p1.cpp
@@ -6,3 +6,19 @@ struct Node {
     string name;
     Node* next;
 };
+
+Node* START = NULL;
+
+void addNode() {
+    int nim;
+    string nama;
+    Node* nodeBaru = new Node();
+    cout << "Masukkan NIM: ";
+    cin >> nim;
+    cout << "Masukkan Nama: ";
+    cin >> nama;
+    nodeBaru->noMhs = nim;
+    nodeBaru->name = nama;
+
+  
+
